6d5aa783c348b220d75b73cfa2a34cd9bb3b2410
kuuku123 <dohyun0047@gmail.com>
2025-01-19 10:57:50 +0900

    init commit

---
+++ src/main/java/org/example/studycafe_webflux/StudyCafeWebFluxApplication.java
@@ -0,0 +1,13 @@
+package org.example.studycafe_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCafeWebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCafeWebFluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/studycafe_webflux/StudyCafeWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studycafe_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCafeWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
