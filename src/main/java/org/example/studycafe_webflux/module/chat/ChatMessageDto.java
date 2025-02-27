95a8dee9c12ea61388bf5d2fb2a31e0c7a1a9142
kuuku123 <dohyun0047@gmail.com>
2025-02-27 12:53:16 +0900

    done: 1) add nickname field at ChatMessageDto

--- src/main/java/org/example/studycafe_webflux/module/chat/ChatMessageDto.java
+++ src/main/java/org/example/studycafe_webflux/module/chat/ChatMessageDto.java
@@ -1,13 +1,14 @@
 package org.example.studycafe_webflux.module.chat;
 
-import lombok.Data;
-
 import java.time.LocalDateTime;
+import lombok.Data;
 
 @Data
 public class ChatMessageDto {
+
     private String id;
     private String studyPath;
+    private String nickname;
     private String email;
     private String text;
     private LocalDateTime createdAt;
